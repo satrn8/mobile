d4d1560d406e690f63b1c6ab3f6ab1176d348262
satrn8 <verypyc@gmail.com>
2022-11-08 14:44:30 +0300

    update test mobile 11/08

--- tests/android/test_labirint_for_local.py
+++ tests/android/test_labirint_for_local.py
@@ -14,60 +14,60 @@ def skip_start_screen(browser):
     time.sleep(2)
 
 
-# def test_id_books():
-#     with allure.step("Тест добавления в корзину книг по ID и заполнение формы заказа"):
-#         skip_start_screen(browser)
-#         browser.element((AppiumBy.ID, 'ru.labirint.android:id/toolbar_search_et')).click()
-#         time.sleep(2)
-#         browser.element((AppiumBy.ID, 'ru.labirint.android.otherfeatures:id/toolbar_search_et')).type("848208")
-#         time.sleep(2)
-#         browser.driver.execute_script("mobile: performEditorAction",  {'action': 'search'})
-#         time.sleep(5)
-#         browser.element((AppiumBy.ID, 'ru.labirint.android.otherfeatures:id/card_product_bottom_panel_btn')).click()
-#         time.sleep(2)
-#
-#         #  закрыть страницу после добавления
-#         browser.element((AppiumBy.ID, 'ru.labirint.android.otherfeatures:id/activity_card_product_close_ib')).click()
-#         time.sleep(2)
-#
-#         #  вернуться на шаг назад
-#         browser.element((AppiumBy.XPATH, '//android.widget.ImageButton[@content-desc="Navigate up"]')).click()
-#         time.sleep(5)
-#         browser.element((AppiumBy.XPATH, '//android.widget.ImageButton[@content-desc="Navigate up"]')).click()
-#         time.sleep(5)
-#
-#         browser.element((AppiumBy.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.RelativeLayout/android.widget.LinearLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.EditText')).click()
-#         time.sleep(2)
-#         browser.element((AppiumBy.ID, 'ru.labirint.android.otherfeatures:id/toolbar_search_et')).type("838271")
-#         time.sleep(2)
-#         browser.driver.execute_script("mobile: performEditorAction", {'action': 'search'})
-#         time.sleep(5)
-#         browser.element((AppiumBy.ID, 'ru.labirint.android.otherfeatures:id/card_product_bottom_panel_btn')).click()
-#         time.sleep(5)
-#         browser.element((AppiumBy.ID, 'ru.labirint.android.otherfeatures:id/card_product_bottom_panel_btn')).click()
-#         time.sleep(5)
-#
-#         browser.element((AppiumBy.ID, 'ru.labirint.android:id/basket_bottom_panel_btn')).click()
-#         time.sleep(10)
-#         browser.element((AppiumBy.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[2]/android.widget.RadioButton')).click()
-#         time.sleep(10)
-#         browser.element((AppiumBy.XPATH, '//android.widget.LinearLayout[@content-desc="список"]')).click()
-#         time.sleep(10)
-#         browser.element((AppiumBy.ID, 'ru.labirint.android.installed_feature_main:id/exw_list_search_et')).click()
-#         browser.element((AppiumBy.ID, 'ru.labirint.android.installed_feature_main:id/exw_list_search_et')).type("1508")
-#         time.sleep(10)
-#         browser.element((AppiumBy.ID, 'ru.labirint.android.installed_feature_main:id/exw_item_rb')).click()
-#         time.sleep(10)
-#
-#
-#         browser.element((AppiumBy.ID, 'ru.labirint.android.installed_feature_main:id/order_temp_phone_et')).type("9998555611")
-#         time.sleep(10)
-#         # browser.element((AppiumBy.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget.FrameLayout/android.widget.EditText')).type("verypyc@test.ru")
-#         browser.element((AppiumBy.ID, 'ru.labirint.android.installed_feature_main:id/order_temp_email_et')).type("verypyc@test.ru")
-#         time.sleep(5)
-#         browser.element((AppiumBy.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout[3]/android.widget.FrameLayout/android.widget.EditText')).type("Алёна")
-#         time.sleep(5)
-#         browser.element((AppiumBy.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout[4]/android.widget.FrameLayout/android.widget.EditText')).type("Иванова")
+def test_id_books():
+    with allure.step("Тест добавления в корзину книг по ID и заполнение формы заказа"):
+        skip_start_screen(browser)
+        browser.element((AppiumBy.ID, 'ru.labirint.android:id/toolbar_search_et')).click()
+        time.sleep(2)
+        browser.element((AppiumBy.ID, 'ru.labirint.android.otherfeatures:id/toolbar_search_et')).type("848208")
+        time.sleep(2)
+        browser.driver.execute_script("mobile: performEditorAction",  {'action': 'search'})
+        time.sleep(5)
+        browser.element((AppiumBy.ID, 'ru.labirint.android.otherfeatures:id/card_product_bottom_panel_btn')).click()
+        time.sleep(2)
+
+        #  закрыть страницу после добавления
+        browser.element((AppiumBy.ID, 'ru.labirint.android.otherfeatures:id/activity_card_product_close_ib')).click()
+        time.sleep(2)
+
+        #  вернуться на шаг назад
+        browser.element((AppiumBy.XPATH, '//android.widget.ImageButton[@content-desc="Navigate up"]')).click()
+        time.sleep(5)
+        browser.element((AppiumBy.XPATH, '//android.widget.ImageButton[@content-desc="Navigate up"]')).click()
+        time.sleep(5)
+
+        browser.element((AppiumBy.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.RelativeLayout/android.widget.LinearLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.EditText')).click()
+        time.sleep(2)
+        browser.element((AppiumBy.ID, 'ru.labirint.android.otherfeatures:id/toolbar_search_et')).type("838271")
+        time.sleep(2)
+        browser.driver.execute_script("mobile: performEditorAction", {'action': 'search'})
+        time.sleep(5)
+        browser.element((AppiumBy.ID, 'ru.labirint.android.otherfeatures:id/card_product_bottom_panel_btn')).click()
+        time.sleep(5)
+        browser.element((AppiumBy.ID, 'ru.labirint.android.otherfeatures:id/card_product_bottom_panel_btn')).click()
+        time.sleep(5)
+
+        browser.element((AppiumBy.ID, 'ru.labirint.android:id/basket_bottom_panel_btn')).click()
+        time.sleep(10)
+        browser.element((AppiumBy.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[2]/android.widget.RadioButton')).click()
+        time.sleep(10)
+        browser.element((AppiumBy.XPATH, '//android.widget.LinearLayout[@content-desc="список"]')).click()
+        time.sleep(10)
+        browser.element((AppiumBy.ID, 'ru.labirint.android.installed_feature_main:id/exw_list_search_et')).click()
+        browser.element((AppiumBy.ID, 'ru.labirint.android.installed_feature_main:id/exw_list_search_et')).type("1508")
+        time.sleep(10)
+        browser.element((AppiumBy.ID, 'ru.labirint.android.installed_feature_main:id/exw_item_rb')).click()
+        time.sleep(10)
+
+
+        browser.element((AppiumBy.ID, 'ru.labirint.android.installed_feature_main:id/order_temp_phone_et')).type("9998555611")
+        time.sleep(10)
+        # browser.element((AppiumBy.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget.FrameLayout/android.widget.EditText')).type("verypyc@test.ru")
+        browser.element((AppiumBy.ID, 'ru.labirint.android.installed_feature_main:id/order_temp_email_et')).type("verypyc@test.ru")
+        time.sleep(5)
+        browser.element((AppiumBy.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout[3]/android.widget.FrameLayout/android.widget.EditText')).type("Алёна")
+        time.sleep(5)
+        browser.element((AppiumBy.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout[4]/android.widget.FrameLayout/android.widget.EditText')).type("Иванова")
 
 
 def test_find():
