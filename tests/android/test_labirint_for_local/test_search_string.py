e9e4f4a359e23fb204074809324954434c5531d4
satrn8 <verypyc@gmail.com>
2022-12-28 23:58:35 +0300

    update test mobile 12/28

---
+++ tests/android/test_labirint_for_local/test_search_string.py
@@ -0,0 +1,22 @@
+import time
+import allure
+from appium.webdriver.common.appiumby import AppiumBy
+from selene.support.shared import browser
+
+
+def skip_start_screen(browser):
+    browser.element((AppiumBy.ID, 'ru.labirint.android.installed_feature_main:id/fragment_main_auth_skip')).click()
+    time.sleep(5)
+    browser.element((AppiumBy.ID, 'ru.labirint.android.installed_feature_main:id/fragment_main_auth_skip')).click()
+    time.sleep(5)
+    browser.element((AppiumBy.XPATH, '//android.widget.TextView[@content-desc="ImageView"]')).click()
+    time.sleep(5)
+
+
+def test_find():
+    with allure.step("Тест поиска на странице"):
+        skip_start_screen(browser)
+        browser.element((AppiumBy.ID, 'ru.labirint.android:id/toolbar_search_et')).click()
+        browser.element((AppiumBy.ID, 'ru.labirint.android.otherfeatures:id/toolbar_search_et')).type("блокнот")
+        browser.driver.execute_script("mobile: performEditorAction",  {'action': 'search'})
+
